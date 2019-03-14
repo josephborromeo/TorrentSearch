a1b87f3eee3480d7217c232288ab26bae815df72
Joseph Borromeo <joseph.borromeo00@gmail.com>
2019-03-13 22:42:29 -0400

    Added Text Box Added status function to determine the status of PC and Plex
    Removed pygame_functions.py
    
    Still need to fix the text box and use a proper font

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
 from urllib.request import urlopen, Request
 import pygame, sys, time, io
 from bs4 import BeautifulSoup
-import pygame_functions
-# pygame_functions taken from https://github.com/StevePaget/Pygame_Functions
 
 """ URLLIB HEADER"""
 hdr = {'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.11 (KHTML, like Gecko) Chrome/23.0.1271.64 Safari/537.11',
@@ -20,8 +18,8 @@ screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 bg_color = (250, 248, 239)
 
 screen.fill(bg_color)
-
-
+pygame.init()
+pygame.key.set_repeat(500, 35)
 # Sets the window name
 pygame.display.set_caption("Torrent Search")
 # Draws all changes to the window
@@ -35,15 +33,15 @@ Movies
 -------
 YIFY: https://yts.am/browse-movies/Search%20term%20here/all/all/0/latest        # Spaces = %20
 
-TV Shows
---------
-EZTV: 
 
 Movies and TV shows
 -------------------
-TPB: 
-KAT: 
-RAR:
+TPB: https://thepiratebay.org/search/Search%20term%20here
+RAR: http://rarbg.to/torrents.php?search=search+term+here
+
+    --- Not using KAT since you cannot eaily dl magnet link ---
+KAT: https://kat2.biz/usearch/search%20term/
+EZTV: Just not a good selection
 
                         FEATURES
                     ----------------
@@ -68,6 +66,14 @@ YIFY:
     - Resolution selector when downloading
 """
 
+"""     STATUS CHECKS       """
+def check_statuses():
+    pc, plex = False, False
+    if os.path.exists("//MEDIA-SERVER/E"):
+        pc = True
+    # TODO: Secondary check for plex
+    return pc, plex
+
 # Array of Movie objects
 movies = []
 
@@ -88,9 +94,9 @@ class Movie:
 # This is necessary in case the domain changes. Need to check all URLs are valid at start of program!
 try:
     start = time.clock()
-    #req = Request(yify("Alice in Wonderland"), headers=hdr)
+    req = Request(yify("Alice in Wonderland"), headers=hdr)
     #req = Request(yify("the"), headers=hdr)
-    req = Request(yify("cars"), headers=hdr)
+    #req = Request(yify("transformers"), headers=hdr)
     html = urlopen(req)
     soup = BeautifulSoup(html.read(), 'html.parser')
 except:
@@ -135,11 +141,65 @@ def show_movies():
         else:
             screen.blit(movies[movie].image, ((SCREEN_WIDTH - ((movies[movie].img_size[0]+10)* num_cols))/2 + (movie - num_cols*2) * (movies[movie].img_size[0]+10), top_padding + 2*(movies[movie].img_size[1]+10)))
 
+
+class InputBox:
+
+    def __init__(self, x, y, w, h, text=''):
+
+        self.rect = pygame.Rect(x, y, w, h)
+        self.color_inactive = (100,100,100)
+        self.color_active = (200,200,200)
+        self.text_color = (0,0,0)
+        self.text = text
+        self.font = pygame.font.SysFont("Arial", 36)
+        self.txt_surface = self.font.render(self.text, True, self.color_inactive)
+        self.active = False
+
+    def handle_event(self, event):
+        if event.type == pygame.MOUSEBUTTONDOWN:
+            # If the user clicked on the input_box rect.
+            if self.rect.collidepoint(event.pos):
+                # Toggle the active variable.
+                self.active = not self.active
+            else:
+                self.active = False
+            # Change the current color of the input box.
+            self.color = self.color_active if self.active else self.color_inactive
+        if event.type == pygame.KEYDOWN:
+            if self.active:
+                if event.key == pygame.K_RETURN:
+                    print(self.text)
+                    self.text = ''
+                elif event.key == pygame.K_BACKSPACE:
+                    self.text = self.text[:-1]
+                else:
+                    self.text += event.unicode
+                # Re-render the text.
+                self.txt_surface = self.font.render(self.text, True, self.text_color)
+
+    def update(self):
+        # Resize the box if the text is too long.
+        width = max(200, self.txt_surface.get_width()+10)
+        self.rect.w = width
+
+    def draw(self, screen):
+        # Blit the rect.
+        if self.active:
+            pygame.draw.rect(screen, (245,245,245), self.rect, 0)
+            pygame.draw.rect(screen, (66, 206, 245), (self.rect.x-2, self.rect.y-2, self.rect.w+2, self.rect.h+2), 3)
+        else:
+            pygame.draw.rect(screen, (245, 245, 245), self.rect, 0)
+
+        # Blit the text.
+        screen.blit(self.txt_surface, (self.rect.x + 5, self.rect.y + 5))
+
+
+textInput = InputBox(5,5,SCREEN_WIDTH/1.2,40, "Search" )
 def draw_header():
     pygame.draw.rect(screen, (40, 40, 40), (0, 0, SCREEN_WIDTH, 80))
-
-    textInput = pygame_functions.makeTextBox(5, 5, SCREEN_WIDTH-450)
-    pygame_functions.showTextBox(textInput)
+    #pygame.draw.rect(screen, (220, 220, 220), (5, 5, SCREEN_WIDTH/1.2, 40))
+    textInput.draw(screen)
+    textInput.update()
 
 running = True
 while running:
@@ -150,6 +210,7 @@ while running:
     draw_header()
     clock.tick(FPS)
     for event in pygame.event.get():
+        textInput.handle_event(event)
         if event.type == pygame.QUIT:
             pygame.quit()
             sys.exit()
